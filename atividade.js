2ebd91b2eb1c07a49cc8d269c08b7309d29a3fdd
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2023-12-06 12:35:25 -0300

    primeiro commit

---
+++ atividade.js
@@ -0,0 +1,20 @@
+function Pessoa(nome, sobrenome, altura, peso, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.altura = altura;
+    this.peso = peso;
+    this.idade = idade;
+
+    this.minhasInfos = function() {
+        console.log(`Nome: ${this.nome} ${this.sobrenome}`);
+        console.log(`Altura: ${this.altura} cm`);
+        console.log(`Peso: ${this.peso} kg`);
+        console.log(`Idade: ${this.idade} anos`);
+    };
+}
+
+const pessoa = new Pessoa('Ramon', 'Galdino', '171', '65', '21');
+
+pessoa.minhasInfos();
+
+// acrescentei o 'Nome' dentro do console.log para ficar melhor apresentado //
